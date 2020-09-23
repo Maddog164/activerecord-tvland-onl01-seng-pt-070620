1e6222949d920246aa0ffbf0254448d271a39fb1
Denise Henderson <denise@henderson3.us>
2020-09-23 08:45:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,11 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
 
+  def actors_list
+    self.actors.collect do |character|
+      actor.full_name
+      # "#{character.name} - #{character.show.name}"
+    end.join
+  end
+
 end
