154f375e307bfdc209f72f255be143839a9d3fab
Denise Henderson <denise@henderson3.us>
2020-09-23 09:03:57 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+
+  belongs_to :network
+  has_many :characters
+
+  def actors_list
+    self.characters.collect do |character|
+      binding.pry
+      character.actor.full_name
+      # "#{character.name} - #{character.show.name}"
+    end
+  end
+
 end
